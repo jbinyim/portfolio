c7a10d900ce5ebf31a474eefa3da24304e2fdc0a
jbinyim12@naver.com <jbinyim12@naver.com>
2024-06-13 18:14:59 +0900

    240613 인터뷰 애니메이션 추가

--- design/src/components/InterviewBox.tsx
+++ design/src/components/InterviewBox.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Container = styled.div`
   width: 100%;
@@ -9,6 +10,7 @@ const Container = styled.div`
   background: ${(props) => props.theme.boxColor};
   border-radius: 30px;
   position: relative;
+  transition: all 0.3s;
   cursor: pointer;
   &:hover {
     background: rgba(150, 150, 150, 0.3);
@@ -23,10 +25,11 @@ const Title = styled.h2`
   }
 `;
 
-const Text = styled.p`
+const Text = styled(motion.p)<{ $toggle: boolean }>`
   margin-top: 30px;
   font-size: 16px;
   line-height: 1.5rem;
+  display: ${(props) => (props.$toggle ? "block" : "none")};
 `;
 
 interface IInterviewBox {
@@ -44,7 +47,14 @@ const InterviewBox = ({ q, a }: IInterviewBox) => {
   return (
     <Container onClick={onClickToggle}>
       <Title>Q. {q}</Title>
-      {toggle ? <Text>{a}</Text> : null}
+      <Text
+        $toggle={toggle}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: toggle ? 1 : 0, y: toggle ? 0 : 20 }}
+        transition={{ duration: 0.3 }}
+      >
+        {a}
+      </Text>
     </Container>
   );
 };
